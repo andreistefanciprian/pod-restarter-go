fd4505da48eef3bcb1a889bc70b8f8b14dd33419
Stefan Andrei <andreistefanciprian@gmail.com>
2023-01-21 21:41:39 +1030

    refactor struct params + add GetPodDetails tests

--- kubernetes/kubernetes_test.go
+++ kubernetes/kubernetes_test.go
@@ -12,16 +12,16 @@ import (
 
 func TestDeletePod(t *testing.T) {
 	testCases := []struct {
-		name            string
-		pods            []runtime.Object
-		targetNamespace string
-		targetPod       string
-		expectSuccess   bool
+		testName      string
+		mockedPods    []runtime.Object
+		podNamespace  string
+		podName       string
+		expectSuccess bool
 	}{
 		// delete a Pod that exists
 		{
-			name: "pod_exists",
-			pods: []runtime.Object{
+			testName: "Delete existing Pod",
+			mockedPods: []runtime.Object{
 				&corev1.Pod{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "pod_exists",
@@ -29,29 +29,29 @@ func TestDeletePod(t *testing.T) {
 					},
 				},
 			},
-			targetNamespace: "default",
-			targetPod:       "pod_exists",
-			expectSuccess:   true,
+			podNamespace:  "default",
+			podName:       "pod_exists",
+			expectSuccess: true,
 		},
 		// delete a Pod that does not exist
 		{
-			name:            "pod_does_not_exist",
-			pods:            []runtime.Object{},
-			targetNamespace: "default",
-			targetPod:       "pod_does_not_exist",
-			expectSuccess:   false,
+			testName:      "Delete Pod that does not exist",
+			mockedPods:    []runtime.Object{},
+			podNamespace:  "default",
+			podName:       "pod_does_not_exist",
+			expectSuccess: false,
 		},
 	}
 
 	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(test.testName, func(t *testing.T) {
 			var clt kubeClient
 			var ctx = context.TODO()
-			clt.clientSet = fake.NewSimpleClientset(test.pods...)
+			clt.clientSet = fake.NewSimpleClientset(test.mockedPods...)
 			err := clt.DeletePod(
 				ctx,
-				test.targetPod,
-				test.targetNamespace,
+				test.podName,
+				test.podNamespace,
 			)
 
 			if err != nil && test.expectSuccess {
@@ -65,18 +65,18 @@ func TestDeletePod(t *testing.T) {
 
 func TestGetEvents(t *testing.T) {
 	testCases := []struct {
-		name                  string
-		events                []runtime.Object
-		namespace             string
+		testName              string
+		mockedEvents          []runtime.Object
+		eventNamespace        string
 		eventReason           string
-		errorMessage          string
+		eventMessage          string
 		expectedPodsWithError int
 	}{
 		// This test is looking for Events that match Reason and Message in a namespace
 		// 1 Event will match Reason and Message
 		{
-			name: "Events that match Reason and Message in a namespace",
-			events: []runtime.Object{
+			testName: "Get Pod Events that match Reason and Message in a namespace",
+			mockedEvents: []runtime.Object{
 				&corev1.Event{
 					ObjectMeta: metav1.ObjectMeta{
 						Namespace: "default",
@@ -153,16 +153,16 @@ func TestGetEvents(t *testing.T) {
 					Type:           corev1.EventTypeWarning,
 				},
 			},
-			namespace:             "default",
+			eventNamespace:        "default",
 			eventReason:           "FailedCreatePodSandBox",
-			errorMessage:          "container veth name provided (eth0) already exists",
+			eventMessage:          "container veth name provided (eth0) already exists",
 			expectedPodsWithError: 1,
 		},
 		// This test is looking for Events that match Reason and Message across all namespaces
 		// 2 Events will match Reason and Message
 		{
-			name: "Events that match Reason and Message in a namespace",
-			events: []runtime.Object{
+			testName: "Get Events that match Reason and Message across all namespaces",
+			mockedEvents: []runtime.Object{
 				&corev1.Event{
 					ObjectMeta: metav1.ObjectMeta{
 						Namespace: "default",
@@ -239,48 +239,100 @@ func TestGetEvents(t *testing.T) {
 					Type:           corev1.EventTypeWarning,
 				},
 			},
-			namespace:             "",
+			eventNamespace:        "",
 			eventReason:           "FailedCreatePodSandBox",
-			errorMessage:          "container veth name provided (eth0) already exists",
+			eventMessage:          "container veth name provided (eth0) already exists",
 			expectedPodsWithError: 2,
 		},
 		// This test is looking for Events that match Reason and Message in a namespace
 		// 0 Events will match Reason and Message
 		{
-			name:                  "No Events in a namespace",
-			events:                []runtime.Object{},
-			namespace:             "default",
+			testName:              "Get no matching Events from namespace",
+			mockedEvents:          []runtime.Object{},
+			eventNamespace:        "default",
 			eventReason:           "FailedCreatePodSandBox",
-			errorMessage:          "container veth name provided (eth0) already exists",
+			eventMessage:          "container veth name provided (eth0) already exists",
 			expectedPodsWithError: 0,
 		},
 		// This test is looking for Events that match Reason and Message across all namespaces
 		// 0 Events will match Reason and Message across all namespaces
 		{
-			name:                  "No Events across all namespaces",
-			events:                []runtime.Object{},
-			namespace:             "",
+			testName:              "Get no matching Events across all namespaces",
+			mockedEvents:          []runtime.Object{},
+			eventNamespace:        "",
 			eventReason:           "FailedCreatePodSandBox",
-			errorMessage:          "container veth name provided (eth0) already exists",
+			eventMessage:          "container veth name provided (eth0) already exists",
 			expectedPodsWithError: 0,
 		},
 	}
 
 	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(test.testName, func(t *testing.T) {
 			var clt kubeClient
 			var ctx = context.TODO()
-			clt.clientSet = fake.NewSimpleClientset(test.events...)
+			clt.clientSet = fake.NewSimpleClientset(test.mockedEvents...)
 			podEvents, err := clt.GetEvents(
 				ctx,
-				test.namespace,
+				test.eventNamespace,
 				test.eventReason,
-				test.errorMessage,
+				test.eventMessage,
 			)
 			if err != nil {
 				t.Fatalf("Unexpected error getting Pod Events: %s", err.Error())
 			} else if test.expectedPodsWithError != len(podEvents) {
 				t.Fatalf("Unexpected Number of Events with Reason and Error Message found. Expected: %d. Found: %d", test.expectedPodsWithError, len(podEvents))
+			}
+		})
+	}
+}
+
+func TestGetPodDetails(t *testing.T) {
+	testCases := []struct {
+		testName      string
+		mockedPods    []runtime.Object
+		podNamespace  string
+		podName       string
+		expectSuccess bool
+	}{
+		// Get the details of an existing Pod
+		{
+			testName: "Pod Exists",
+			mockedPods: []runtime.Object{
+				&corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "default",
+					},
+				},
+			},
+			podNamespace:  "default",
+			podName:       "foo",
+			expectSuccess: true,
+		},
+		// Get the details of a Pod that does not exist
+		{
+			testName:      "Pod does not exist",
+			mockedPods:    []runtime.Object{},
+			podNamespace:  "default",
+			podName:       "foo",
+			expectSuccess: false,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.testName, func(t *testing.T) {
+			var clt kubeClient
+			var ctx = context.TODO()
+			clt.clientSet = fake.NewSimpleClientset(test.mockedPods...)
+			_, err := clt.GetPodDetails(
+				ctx,
+				test.podName,
+				test.podNamespace,
+			)
+			if err != nil && test.expectSuccess {
+				t.Fatalf("Unexpected error geting existing Pod: %s", err.Error())
+			} else if err == nil && !test.expectSuccess {
+				t.Fatalf("We we're expecting an Error for getting details of a Pod that does not exist!")
 			}
 		})
 	}
